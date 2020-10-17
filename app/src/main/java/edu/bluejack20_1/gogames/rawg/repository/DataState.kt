1d4d6809c364f08d7b6bc21de431f8c794424107
Martin <martinwijaya2013@gmail.com>
2020-10-17 20:44:23 +0700

    community

--- app/src/main/java/edu/bluejack20_1/gogames/rawg/repository/DataState.kt
+++
@@ -1,7 +0,0 @@
-package edu.bluejack20_1.gogames.rawg.repository
-
-sealed class DataState<T> {
-    data class Error<T>(val error:String): DataState<T>()
-    data class NetworkError<T>(val message:String): DataState<T>()
-    data class Success<T>(val data: T?): DataState<T>()
-}
--- app/src/main/java/edu/bluejack20_1/gogames/rawg/ui/details/viewmodel/DetailViewIntent.kt
+++
@@ -1,5 +0,0 @@
-package edu.bluejack20_1.gogames.rawg.ui.details.viewmodel
-
-sealed class DetailViewIntent {
-    data class FetchData(val gameID: String): DetailViewIntent()
-}
